1209243a723a0561de1f7a795b3290ce85916065
Felipe Alencar <felipealencarlopes@gmail.com>
2018-05-23 20:13:18 -0300

    - Testing.

--- app/simple_monitor_13_to_csv.py
+++ app/simple_monitor_13_to_csv.py
@@ -77,9 +77,10 @@ class SimpleMonitor13(simple_switch_stp_13.SimpleSwitch13):
         #self.logger.info('---------------- '
         #                 '-------- ----------------- '
         #                 '-------- -------- --------')
-        if ev.msg.datapath.id == "0000000000000002":
+        datapath_id = "%016x" % ev.msg.datapath.id
+        if datapath_id == "0000000000000002":
             self.logger.info(body)
-        for stat in sorted([flow for flow in body if flow.priority == 1],
+        for stat in sorted([flow for flow in body],
                            key=lambda flow: (flow.match['in_port'],
                                              flow.match['eth_dst'])):
             #self.logger.info('%016x %8x %17s %8x %8d %8d %d %d 0x%04x',
