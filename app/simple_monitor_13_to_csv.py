109b9ab62f091f4e26d7821b3b69448da6568194
Felipe Alencar <felipealencarlopes@gmail.com>
2018-05-24 06:02:07 -0300

    - Fixing.

--- app/simple_monitor_13_to_csv.py
+++ app/simple_monitor_13_to_csv.py
@@ -99,9 +99,9 @@ class SimpleMonitor13(simple_switch_stp_13.SimpleSwitch13):
                 datapath_id = "%016x" % ev.msg.datapath.id
                 inport = "%8x" % stat.match['in_port']
                 eth_dst = "%17s" % stat.match['eth_dst']
-                vlan = "%8x" % stat.instructions[0].actions[0].field['OXMTlv'].value
+                vlan = "%8x" % stat.instructions[0].actions[0].vlan_vid
                 print ('vlan', vlan)
-                outport = "%8x" % stat.instructions[1].actions[0].port
+                outport = "%8x" % stat.instructions[0].actions[1].port
                 packets = "%8d" % stat.packet_count
                 bytes = "%8d" % stat.byte_count
                 duration_sec = "%d" % stat.duration_sec
