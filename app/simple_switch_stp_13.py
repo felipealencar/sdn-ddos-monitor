25ffa005a1bb739fd8a0421885d2340af45f4f6b
Felipe Alencar <felipealencarlopes@gmail.com>
2018-05-24 04:55:34 -0300

    - Fixing ip check.

--- app/simple_switch_stp_13.py
+++ app/simple_switch_stp_13.py
@@ -85,12 +85,14 @@ class SimpleSwitch13(simple_switch_13.SimpleSwitch13):
             out_port = ofproto.OFPP_FLOOD
 
         ip = pkt.get_protocols(ipv4.ipv4)
-        print ip
-        #if ip and ip.src != '10.0.0.1':
-        #    print('entrou')
-        #    actions = [parser.OFPActionSetField(vlan_vid=1), parser.OFPActionOutput(out_port)]
-        #else:
-        actions = [parser.OFPActionOutput(out_port)]
+        if ip:
+            if ip.src == '10.0.0.1':
+                print('entrou')
+                actions = [parser.OFPActionSetField(vlan_vid=1), parser.OFPActionOutput(out_port)]
+            else:
+                actions = [parser.OFPActionOutput(out_port)]
+        else:
+            actions = [parser.OFPActionOutput(out_port)]
 
         # install a flow to avoid packet_in next time
         if out_port != ofproto.OFPP_FLOOD:
